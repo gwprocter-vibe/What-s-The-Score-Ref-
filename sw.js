fb3fbd6270cb25af4477101bcc2b08cb72f4a682
gwprocter-vibe <gwprocter@googlemail.com>
2026-09-14 18:06:55 +0100

    Restore clean bottom brand signature (MATCHDAY COMPANION) and bump SW cache to v57

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // What's The Score Ref - 100% Offline Service Worker
-const CACHE_NAME = 'whatsthescoreref-v56';
+const CACHE_NAME = 'whatsthescoreref-v57';
 const STATIC_ASSETS = [
   './',
   './index.html',
